3aac78e719309ce5d71574c85928479a9b29a5c3
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-06-25 07:39:49 +0530

    change

--- frontend/src/constants/productConstant.js
+++ frontend/src/constants/productConstant.js
@@ -3,8 +3,7 @@ export const PRODUCT_LIST_SUCCESS = "PRODUCT_LIST_SUCCESS";
 export const PRODUCT_LIST_FAIL = "PRODUCT_LIST_FAIL";
 
 
-export const PRODUCT_LIST_reset = "PRODUCT_LIST_reset";
-//
+
 
 
 export const PRODUCT_DETAILS_REQUEST = "PRODUCT_DETAILS_REQUEST";
