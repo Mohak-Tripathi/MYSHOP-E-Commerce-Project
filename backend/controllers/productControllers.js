f2658d89c6fd5aaf9946b00ff7ca53ad9d100265
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-06-29 12:08:21 +0530

    done

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -5,7 +5,7 @@ import Product from "../models/productModel.js";
 // @route   GET /api/products
 // @access  Public
 const getProducts = expressAsyncHandler(async (req, res) => {
-  const pageSize = 5;
+  const pageSize = 8;
   const page = Number(req.query.pageNumber) || 1; //?pageNumber=1
 
   const keyword = req.query.keyword
